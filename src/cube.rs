71f2170710e5d176449b7cbb28562b35604650a4
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2024-09-20 09:30:40 +0100

    Update to latest disk-based-bfs, and use settings from megaminx <U,R> run

--- src/cube.rs
+++ src/cube.rs
@@ -1,6 +1,11 @@
 use std::path::PathBuf;
 
-use disk_based_bfs::{bfs::Bfs, callback::BfsCallback, io::LockedIO, settings::BfsSettingsBuilder};
+use disk_based_bfs::{
+    bfs::Bfs,
+    callback::BfsCallback,
+    io::LockedIO,
+    settings::{BfsSettingsBuilder, BfsSettingsProvider, ChunkFilesBehavior, UpdateFilesBehavior},
+};
 use tracing_subscriber::{layer::SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter};
 
 const EP_SIZE: u32 = 181440;
@@ -315,6 +320,22 @@ impl BfsCallback for Callback {
     fn end_of_chunk(&self, _: usize, _: usize) {}
 }
 
+struct SettingsProvider;
+
+impl BfsSettingsProvider for SettingsProvider {
+    fn chunk_root_idx(&self, chunk_idx: usize) -> usize {
+        [0, 1, 2, 3, 1, 2, 3][chunk_idx % 7]
+    }
+
+    fn update_files_behavior(&self, _: usize) -> UpdateFilesBehavior {
+        UpdateFilesBehavior::CompressAndKeep
+    }
+
+    fn chunk_files_behavior(&self, _: usize) -> ChunkFilesBehavior {
+        ChunkFilesBehavior::Keep
+    }
+}
+
 pub fn main() {
     tracing_subscriber::registry()
         .with(
@@ -338,21 +359,25 @@ pub fn main() {
         .threads(48)
         // 4 * 48 chunks
         .chunk_size_bytes(434010150)
-        .update_memory(1 << 37)
+        .update_memory(112 * (1 << 30))
+        .num_update_blocks(2 * 4 * 48 * 48)
         .capacity_check_frequency(256)
         .initial_states(&[solved])
         .state_size(666639590400)
         .root_directories(&[
+            PathBuf::from("/media/ben/drive1/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive2/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive3/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive4/bfs/3x3-U-R-F2/"),
         ])
         .initial_memory_limit(1 << 28)
-        .update_files_compression_threshold(3 * (1 << 32))
-        .buf_io_capacity(1 << 23)
-        .use_locked_io(true)
+        .available_disk_space_limit(256 * (1 << 30))
+        .update_array_threshold(434010150)
+        .use_locked_io(false)
         .sync_filesystem(true)
-        .compress_update_files_at_end_of_iter(true)
+        .compute_checksums(true)
+        .compress_bit_arrays(true)
+        .settings_provider(SettingsProvider)
         .build()
         .unwrap();
 
@@ -380,6 +405,7 @@ pub fn main() {
     let locked_io = LockedIO::new(
         &settings,
         vec![
+            PathBuf::from("/media/ben/drive1/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive2/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive3/bfs/3x3-U-R-F2/"),
             PathBuf::from("/media/ben/drive4/bfs/3x3-U-R-F2/"),
