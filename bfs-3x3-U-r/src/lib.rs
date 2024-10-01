fa2ed5cfc81474f04f7363b11f6e59f703a30293
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2024-10-01 17:21:39 +0100

    Add QTM support to 3x3 <U,r>

--- bfs-3x3-U-r/src/lib.rs
+++ bfs-3x3-U-r/src/lib.rs
@@ -16,15 +16,22 @@ mod coord_cube;
 mod cube;
 mod transposition_tables;
 
-const EXPANSION_NODES: usize = 6;
+const EXPANSION_NODES_HTM: usize = 6;
+const EXPANSION_NODES_QTM: usize = 4;
+
+const CALLBACK_BOUND_HTM: usize = 30;
+const CALLBACK_BOUND_QTM: usize = 37;
+
+const PROVIDER_BOUND_HTM: usize = 20;
+const PROVIDER_BOUND_QTM: usize = 25;
 
 #[derive(Clone)]
-struct Expander<'a> {
+struct ExpanderHtm<'a> {
     cube: CoordCube<'a>,
 }
 
-impl BfsExpander<EXPANSION_NODES> for Expander<'_> {
-    fn expand(&mut self, node: u64, expanded_nodes: &mut [u64; EXPANSION_NODES]) {
+impl BfsExpander<EXPANSION_NODES_HTM> for ExpanderHtm<'_> {
+    fn expand(&mut self, node: u64, expanded_nodes: &mut [u64; EXPANSION_NODES_HTM]) {
         self.cube.decode(node);
         self.cube.u();
         expanded_nodes[0] = self.cube.encode();
@@ -43,11 +50,33 @@ impl BfsExpander<EXPANSION_NODES> for Expander<'_> {
 }
 
 #[derive(Clone)]
-struct Callback;
+struct ExpanderQtm<'a> {
+    cube: CoordCube<'a>,
+}
+
+impl BfsExpander<EXPANSION_NODES_QTM> for ExpanderQtm<'_> {
+    fn expand(&mut self, node: u64, expanded_nodes: &mut [u64; EXPANSION_NODES_QTM]) {
+        self.cube.decode(node);
+        self.cube.u();
+        expanded_nodes[0] = self.cube.encode();
+        self.cube.u();
+        self.cube.u();
+        expanded_nodes[1] = self.cube.encode();
+        self.cube.u();
+        self.cube.rw();
+        expanded_nodes[2] = self.cube.encode();
+        self.cube.rw();
+        self.cube.rw();
+        expanded_nodes[3] = self.cube.encode();
+    }
+}
+
+#[derive(Clone)]
+struct Callback(usize);
 
 impl BfsCallback for Callback {
     fn new_state(&mut self, depth: usize, state: u64) {
-        if depth >= 30 {
+        if depth >= self.0 {
             tracing::info!("depth {depth} state {state}");
         }
     }
@@ -55,7 +84,7 @@ impl BfsCallback for Callback {
     fn end_of_chunk(&self, _: usize, _: usize) {}
 }
 
-struct Provider;
+struct Provider(usize);
 
 impl BfsSettingsProvider for Provider {
     fn chunk_root_idx(&self, chunk_idx: usize) -> usize {
@@ -63,7 +92,7 @@ impl BfsSettingsProvider for Provider {
     }
 
     fn update_files_behavior(&self, depth: usize) -> UpdateFilesBehavior {
-        if depth >= 20 {
+        if depth >= self.0 {
             UpdateFilesBehavior::MergeAndKeep
         } else {
             UpdateFilesBehavior::DontMerge
@@ -71,7 +100,7 @@ impl BfsSettingsProvider for Provider {
     }
 
     fn chunk_files_behavior(&self, depth: usize) -> ChunkFilesBehavior {
-        if depth >= 20 {
+        if depth >= self.0 {
             ChunkFilesBehavior::Keep
         } else {
             ChunkFilesBehavior::Delete
@@ -79,7 +108,13 @@ impl BfsSettingsProvider for Provider {
     }
 }
 
-pub fn run() {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Metric {
+    Htm,
+    Qtm,
+}
+
+pub fn run(metric: Metric) {
     tracing_subscriber::registry()
         .with(
             EnvFilter::try_from_default_env()
@@ -96,32 +131,41 @@ pub fn run() {
 
     let transposition_tables = TranspositionTables::new();
 
-    BfsBuilder::new()
-        .threads(48)
-        .chunk_size_bytes(529079040)
-        .update_memory(112 * (1 << 30))
-        .num_update_blocks(2 * 48 * 1280)
-        .capacity_check_frequency(256)
-        .initial_states(&[CoordCube::new(&transposition_tables).encode()])
-        .state_size(5417769369600)
-        .root_directories(&[
-            PathBuf::from("/media/ben/drive1/bfs/3x3-U-r/"),
-            PathBuf::from("/media/ben/drive2/bfs/3x3-U-r/"),
-            PathBuf::from("/media/ben/drive3/bfs/3x3-U-r/"),
-            PathBuf::from("/media/ben/drive4/bfs/3x3-U-r/"),
-        ])
-        .initial_memory_limit(1 << 34)
-        .available_disk_space_limit(256 * (1 << 30))
-        .update_array_threshold(529079040)
-        .use_locked_io(false)
-        .sync_filesystem(true)
-        .compute_checksums(true)
-        .use_compression(true)
-        .expander(Expander {
-            cube: CoordCube::new(&transposition_tables),
-        })
-        .callback(Callback)
-        .settings_provider(Provider)
-        .run_no_defaults()
-        .unwrap();
+    macro_rules! run {
+        ($expander:ident, $callback_bound:expr, $provider_bound:expr) => {
+            BfsBuilder::new()
+                .threads(48)
+                .chunk_size_bytes(529079040)
+                .update_memory(112 * (1 << 30))
+                .num_update_blocks(2 * 48 * 1280)
+                .capacity_check_frequency(256)
+                .initial_states(&[CoordCube::new(&transposition_tables).encode()])
+                .state_size(5417769369600)
+                .root_directories(&[
+                    PathBuf::from("/media/ben/drive1/bfs/3x3-U-r/"),
+                    PathBuf::from("/media/ben/drive2/bfs/3x3-U-r/"),
+                    PathBuf::from("/media/ben/drive3/bfs/3x3-U-r/"),
+                    PathBuf::from("/media/ben/drive4/bfs/3x3-U-r/"),
+                ])
+                .initial_memory_limit(1 << 34)
+                .available_disk_space_limit(256 * (1 << 30))
+                .update_array_threshold(529079040)
+                .use_locked_io(false)
+                .sync_filesystem(true)
+                .compute_checksums(true)
+                .use_compression(true)
+                .expander($expander {
+                    cube: CoordCube::new(&transposition_tables),
+                })
+                .callback(Callback($callback_bound))
+                .settings_provider(Provider($provider_bound))
+                .run_no_defaults()
+                .unwrap()
+        };
+    }
+
+    match metric {
+        Metric::Htm => run!(ExpanderHtm, CALLBACK_BOUND_HTM, PROVIDER_BOUND_HTM),
+        Metric::Qtm => run!(ExpanderQtm, CALLBACK_BOUND_QTM, PROVIDER_BOUND_QTM),
+    }
 }
